58186e0b31e030f15cf2a694c968b0111d964f40
rfcarmo <rfortunatoc@gmail.com>
2025-08-12 17:02:38 +0100

    New project files.

---
+++ src/main/java/com/algaworks/algasensors/tempmonitoring/TempMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algasensors.tempmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempMonitoringApplication.class, args);
+	}
+
+}
